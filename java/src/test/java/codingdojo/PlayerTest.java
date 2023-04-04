679914e76185957512635fb59c34a6157e60ee0d
Florian Graff <florian.graff@klosebrothers.de>
2023-04-04 19:33:03 +0200

    handover

--- java/src/test/java/codingdojo/PlayerTest.java
+++ java/src/test/java/codingdojo/PlayerTest.java
@@ -5,23 +5,31 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class PlayerTest {
 
     // choose this one if you are familiar with mocks
-    @Disabled("Test is not finished yet")
+//    @Disabled("Test is not finished yet")
     @Test
     void damageCalculationsWithMocks() {
+//        given
         Inventory inventory = mock(Inventory.class);
+        Equipment equipment = mock(Equipment.class);
+        when(inventory.getEquipment()).thenReturn(equipment);
         Stats stats = mock(Stats.class);
         SimpleEnemy target = mock(SimpleEnemy.class);
+        final Player player = new Player(inventory, stats);
 
-        Damage damage = new Player(inventory, stats).calculateDamage(target);
+//        when
+        Damage damage = player.calculateDamage(target);
+
+//        then
         assertEquals(10, damage.getAmount());
     }
 
     // choose this one if you are not familiar with mocks
-    @Disabled("Test is not finished yet")
+//    @Disabled("Test is not finished yet")
     @Test
     void damageCalculations() {
         Inventory inventory = new Inventory(null);
